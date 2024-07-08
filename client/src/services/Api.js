904a98816eeb74a1be0e9542cf02509b405a101b
wengangfei@gmail.com <wengangfei@gmail.com>
2024-07-08 16:59:55 -0400

    added

---
+++ client/src/services/Api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default()=>{
+    //return a connector
+    return axios.create({
+        //point to backend url of 8080
+        baseURL:'http://localhost:8080/'
+    })
+}
