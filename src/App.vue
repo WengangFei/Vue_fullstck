243b6303e544e0f3f87f5efe8fe9c93e36afb5dc
wengangfei@gmail.com <wengangfei@gmail.com>
2024-07-08 14:46:26 -0400

    aetup

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,5 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="Hi Fei" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>New Project</h1>
 </template>
